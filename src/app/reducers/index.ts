d0cea6f966a70847f8643505ca488130cfe6a199
Sumanth Venkata <fa2688>
2020-06-07 21:19:16 -0400

    added router dev tools and metareducers

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,14 +6,23 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { routerReducer } from '@ngrx/router-store';
 
 export interface AppState {
 
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-
+  router:routerReducer
 };
 
+export function logger(reducer:ActionReducer<any>):ActionReducer<any>{
+    return (state,action)=> {
+      console.log('sate beforeee ..',state);
+      console.log('Action beforeee ..',action);
+      return reducer(state,action)
+    }
+}
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [logger] : [];
